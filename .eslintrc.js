b3a53a281b90f0802f410b9335eaf67a016ad235
ImUrX <urielfontan2002@gmail.com>
2020-08-21 01:23:20 -0300

    starting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ www/worker.js
@@ -0,0 +1,35 @@
+/* eslint-env worker */
+/* global wasm_bindgen:readonly */
+importScripts("./wasm/libenchcrack.js");
+
+const { Cracker } = wasm_bindgen;
+
+self.onmessage = event => {
+    let cracker = wasm_bindgen("./wasm/libenchcrack_bg.wasm").then(() => 
+        new Cracker(...event.data)
+    ).catch(err => {
+        setTimeout(() => {
+            throw err;
+        });
+        throw err;
+    });
+
+    self.onmessage = async event => {
+        // This will queue further commands up until the module is fully initialised:
+        await cracker;
+        if(event.data.length === 4) {
+            cracker.add_input(...event.data);
+        } else if (event.data.length === 8) {
+            cracker.first_input(...event.data);
+        } else if(event.data.length === 1) {
+            switch (event.data[0]) {
+            case "remaining":
+                postMessage(cracker.possible_seeds());
+                break;
+            case "reset":
+                cracker.reset();
+                postMessage(true);
+            }
+        }
+    };
+};
