40a7fe155568ef7bff7cc8a272a19f6e8d9f88a9
ImUrX <urielfontan2002@gmail.com>
2020-08-22 02:37:03 -0300

    some progress

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,16 @@
 import { WorkerPool } from "./pool.js";
-const pool = new WorkerPool();
+const pool = new WorkerPool("../worker.js");
 window.pool = pool;
 
-window.onload = () => {
-    
+window.onload = async () => {
+
+    //Crack seed
+    document.querySelector("#calc-seed").addEventListener("submit", ev => {
+        ev.preventDefault();
+        const seeds = [
+            parseInt(document.querySelector("#seed1").value, 16),
+            parseInt(document.querySelector("#seed2").value, 16)
+        ];
+        console.log(seeds);
+    });
 };
